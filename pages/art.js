a45ff9b82573aaf8061797b3a964bd2efe810ba8
dhaidahoumaima <oumaima.dhaidah1sm2016@gmail.com>
2023-05-25 16:21:16 +0200

    art modifications

---
+++ pages/art.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CardArtWithDetails from "components/Cards/CardArtWithDetails";
+
+
+export default function Art() {
+    return (
+      <>
+        <div>
+      <CardArtWithDetails />
+    </div>
+      </>
+    );
+  }
+  
