5c45b5cc211e04c653e8328d37066f2521645807
bgerelmaa <gerelmaa.142@gmail.com>
2023-05-27 01:06:47 +0200

    added map markers

--- components/Maps/MapExample.js
+++ components/Maps/MapExample.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import markersData from './markers';
 
 function MapExample() {
+  
+  
+const locations = [
+  { lat: -31.56391, lng: 147.154312 },
+  { lat: -33.718234, lng: 150.363181 },
+  { lat: -33.727111, lng: 150.371124 },
+  { lat: -33.848588, lng: 151.209834 },
+  { lat: -33.851702, lng: 151.216968 },
+  { lat: -34.671264, lng: 150.863657 },
+  { lat: -35.304724, lng: 148.662905 },
+  { lat: -36.817685, lng: 175.699196 },
+  { lat: -36.828611, lng: 175.790222 },
+  { lat: -37.75, lng: 145.116667 },
+  { lat: -37.759859, lng: 145.128708 },
+  { lat: -37.765015, lng: 145.133858 },
+  { lat: -37.770104, lng: 145.143299 },
+  { lat: -37.7737, lng: 145.145187 },
+  { lat: -37.774785, lng: 145.137978 },
+  { lat: -37.819616, lng: 144.968119 },
+  { lat: -38.330766, lng: 144.695692 },
+  { lat: -39.927193, lng: 175.053218 },
+  { lat: -41.330162, lng: 174.865694 },
+  { lat: -42.734358, lng: 147.439506 },
+  { lat: -42.734358, lng: 147.501315 },
+  { lat: -42.735258, lng: 147.438 },
+  { lat: -43.999792, lng: 170.463352 },
+];
+  const [dataArray, setDataArray] = useState([]);
   const mapRef = React.useRef(null);
-  React.useEffect(() => {
-    let google = window.google;
-    let map = mapRef.current;
-    let lat = "40.748817";
-    let lng = "-73.985428";
-    const myLatlng = new google.maps.LatLng(lat, lng);
-    const mapOptions = {
+    useEffect(() => {
+      let google = window.google;
+      let map = mapRef.current;
+      let lat = "47.4979";
+      let lng = "19.0402";
+      const myLatlng = new google.maps.LatLng(lat, lng);
+      const mapOptions = {
       zoom: 12,
       center: myLatlng,
       scrollwheel: false,
@@ -64,6 +93,37 @@ function MapExample() {
       map: map,
       animation: google.maps.Animation.DROP,
       title: "Notus NextJS!",
+    });
+
+     // Fetch the marker data from the JSON file
+    //  fetch("./markers.json")
+    //  .then(response => response.json())
+    //  .then(data => {
+    //   console.log("jijij");
+    //    // Loop through the marker data and create markers on the map
+    //    data.forEach(marker => {
+    //       google.maps.event.addListener(marker, "click", function () {
+    //         infowindow.open(map, marker);
+    //       });
+    //    });
+    //  })
+    //  .catch(error => {
+    //    console.error('Error fetching marker data:', error);
+    //  });
+
+    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    const markers = locations.map((position, i) => {
+      marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        animation: google.maps.Animation.DROP,
+        title: "i",
+      });
+
+      google.maps.event.addListener(marker, "click", function () {
+        infowindow.open(map, marker);
+      });
     });
 
     const contentString =
